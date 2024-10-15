79459289cc3cf36bf6755645c97cdf19f9debe27
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-15 20:59:34 +0800

    feat : block and hiden pengguna role staff

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -11,6 +11,12 @@ class PenggunaController extends Controller
     /**
      * Display a listing of the resource.
      */
+
+    public function __construct()
+    {
+        $this->middleware('role:staff');
+    }
+
     public function index()
     {
         $users = DB::table('users')
