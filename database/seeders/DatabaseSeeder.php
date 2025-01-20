1046f54ff612bd1dc4bd0b8524ecb26df41f15e4
Palguna1121 <palguna2212@gmail.com>
2025-01-20 22:20:07 +0800

    feat: add CabangSeeder and integrate with DatabaseSeeder, update TransaksiPengeluaranSeeder to include cabang_id

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PermissionSeeder::class,
             ProdukSeeder::class,
+            CabangSeeder::class,
             TransaksiPengeluaranSeeder::class,
             TargetPenjualanSeeder::class,
             MasterBonusSeeder::class,
